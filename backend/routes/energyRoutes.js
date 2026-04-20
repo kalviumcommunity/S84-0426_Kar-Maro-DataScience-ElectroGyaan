6b9da4d2e20218b718b914a56f61465fb09bc077
kartikay-28 <kartikayrattan1@gmail.com>
2026-04-20 14:46:26 +0530

    feat: PR 6 Integrate ML Service Anomaly Detection and Alerts API

--- backend/routes/energyRoutes.js
+++ backend/routes/energyRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { ingestEnergyData, getUsers, getEnergyData } from '../controllers/energyController.js';
+import { ingestEnergyData, getUsers, getEnergyData, getAlerts } from '../controllers/energyController.js';
 
 const router = express.Router();
 
 router.get('/users', getUsers);
 router.post('/ingest', ingestEnergyData);
 router.get('/:userId', getEnergyData);
+router.get('/:userId/alerts', getAlerts);
 
 export default router;
